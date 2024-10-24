f8ae18a27722dffedc33bb1b69881d1001d8fa14
JC <jianmin0chen@gmail.com>
2024-10-24 14:30:51 -0400

    Tabs to spaces

--- zig/build.zig
+++ zig/build.zig
@@ -3,51 +3,51 @@ const std = @import("std");
 const Build = std.Build;
 
 pub fn build(b: *Build) !void {
-	const target = b.standardTargetOptions(.{});
-	const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-	const main = b.addExecutable(.{
-		.name = "hackcraft",
-		.root_source_file = b.path("src/main.zig"),
-		.target = target,
-		.optimize = optimize
-	});
+    const main = b.addExecutable(.{
+        .name = "hackcraft",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize
+    });
 
-	const math = b.addModule("math", .{
-		.root_source_file = b.path("src/math/root.zig"),
-		.target = target,
-		.optimize = optimize
-	});
+    const math = b.addModule("math", .{
+        .root_source_file = b.path("src/math/root.zig"),
+        .target = target,
+        .optimize = optimize
+    });
 
-	math.addIncludePath(Build.LazyPath{ .cwd_relative = "/opt/homebrew/Cellar/glfw/3.4/include" });
-	math.addLibraryPath(Build.LazyPath{ .cwd_relative = "/opt/homebrew/Cellar/glfw/3.4/lib" });
+    math.addIncludePath(Build.LazyPath{ .cwd_relative = "/opt/homebrew/Cellar/glfw/3.4/include" });
+    math.addLibraryPath(Build.LazyPath{ .cwd_relative = "/opt/homebrew/Cellar/glfw/3.4/lib" });
 
-	math.addIncludePath(Build.LazyPath{ .cwd_relative = "./deps" });
+    math.addIncludePath(Build.LazyPath{ .cwd_relative = "./deps" });
 
-	math.linkFramework("OpenGL", .{});
-	math.linkSystemLibrary("glfw", .{});
+    math.linkFramework("OpenGL", .{});
+    math.linkSystemLibrary("glfw", .{});
 
-	main.root_module.addImport("math", math);
+    main.root_module.addImport("math", math);
 
-	main.addIncludePath(Build.LazyPath{ .cwd_relative = "/opt/homebrew/Cellar/glfw/3.4/include" });
-	main.addLibraryPath(Build.LazyPath{ .cwd_relative = "/opt/homebrew/Cellar/glfw/3.4/lib" });
+    main.addIncludePath(Build.LazyPath{ .cwd_relative = "/opt/homebrew/Cellar/glfw/3.4/include" });
+    main.addLibraryPath(Build.LazyPath{ .cwd_relative = "/opt/homebrew/Cellar/glfw/3.4/lib" });
 
-	main.addIncludePath(Build.LazyPath{ .cwd_relative = "./deps" });
-	main.addCSourceFile(.{
-		.file = b.path("./deps/glad.c"),
-		.flags = &.{}
-	});
-	main.addCSourceFile(.{
-		.file = b.path("./deps/stb.c"),
-		.flags = &.{}
-	});
+    main.addIncludePath(Build.LazyPath{ .cwd_relative = "./deps" });
+    main.addCSourceFile(.{
+        .file = b.path("./deps/glad.c"),
+        .flags = &.{}
+    });
+    main.addCSourceFile(.{
+        .file = b.path("./deps/stb.c"),
+        .flags = &.{}
+    });
 
-	main.linkFramework("OpenGL");
-	main.linkSystemLibrary("glfw");
+    main.linkFramework("OpenGL");
+    main.linkSystemLibrary("glfw");
 
-	b.installArtifact(main);
+    b.installArtifact(main);
 
-	const run_exe = b.addRunArtifact(main);
-	const run_step = b.step("run", "Run");
-	run_step.dependOn(&run_exe.step);
+    const run_exe = b.addRunArtifact(main);
+    const run_step = b.step("run", "Run");
+    run_step.dependOn(&run_exe.step);
 }
--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,6 +1,6 @@
 const c = @cImport({
-	@cInclude("glad/glad.h");
-	@cInclude("GLFW/glfw3.h");
+    @cInclude("glad/glad.h");
+    @cInclude("GLFW/glfw3.h");
 });
 const std = @import("std");
 const math = @import("math");
@@ -13,117 +13,117 @@ const MatrixPrimitive = math.MatrixPrimitive;
 const FLOAT = math.constants.FLOAT;
 
 const Options = struct {
-	initial_width: c_int = 1000,
-	initial_height: c_int = 700,
+    initial_width: c_int = 1000,
+    initial_height: c_int = 700,
 
-	width: c_int = 1000,
-	height: c_int = 700,
+    width: c_int = 1000,
+    height: c_int = 700,
 
-	fov: FLOAT = 60,
-	near: FLOAT = -5,
-	far: FLOAT = 100,
-	perspective: MatrixPrimitive = undefined,
+    fov: FLOAT = 60,
+    near: FLOAT = -5,
+    far: FLOAT = 100,
+    perspective: MatrixPrimitive = undefined,
 
-	// pub fn from(path: []const u8) !Self {
-	// }
+    // pub fn from(path: []const u8) !Self {
+    // }
 };
 
 pub fn main() !void {
-	var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-	defer std.debug.assert(gpa.deinit() == .ok);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
 
-	const allocator = gpa.allocator();
+    const allocator = gpa.allocator();
 
-	var options = Options{};
+    var options = Options{};
 
-	if (c.glfwInit() == c.GL_FALSE)
-		return error.InitializationError;
-	c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 3);
-	c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
-	if (comptime @import("builtin").os.tag == .macos)
-		c.glfwWindowHint(c.GLFW_OPENGL_FORWARD_COMPAT, c.GL_TRUE);
-	
-	const window = c.glfwCreateWindow(
-		options.initial_width,
-		options.initial_height,
-		"Hackcraft",
-		null,
-		null
-	);
-	if (window == null) {
-		c.glfwTerminate();
-		return error.InitializationError;
-	}
-	c.glfwMakeContextCurrent(window);
-	_ = c.glfwSetFramebufferSizeCallback(window, resize);
+    if (c.glfwInit() == c.GL_FALSE)
+        return error.InitializationError;
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 3);
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
+    if (comptime @import("builtin").os.tag == .macos)
+        c.glfwWindowHint(c.GLFW_OPENGL_FORWARD_COMPAT, c.GL_TRUE);
+    
+    const window = c.glfwCreateWindow(
+        options.initial_width,
+        options.initial_height,
+        "Hackcraft",
+        null,
+        null
+    );
+    if (window == null) {
+        c.glfwTerminate();
+        return error.InitializationError;
+    }
+    c.glfwMakeContextCurrent(window);
+    _ = c.glfwSetFramebufferSizeCallback(window, resize);
 
-	if (
-		c.gladLoadGLLoader(
-			@ptrCast(&c.glfwGetProcAddress)
-		) == c.GL_FALSE
-	) return error.InitializationError;
+    if (
+        c.gladLoadGLLoader(
+            @ptrCast(&c.glfwGetProcAddress)
+        ) == c.GL_FALSE
+    ) return error.InitializationError;
 
-	// c.glEnable(c.GL_CULL_FACE);
-	// c.glEnable(c.GL_DEPTH);
-	// c.glEnable(c.GL_BLEND);
-	// c.glBlendFunc(c.GL_SRC_ALPHA, c.GL_ONE_MINUS_SRC_ALPHA);
+    // c.glEnable(c.GL_CULL_FACE);
+    // c.glEnable(c.GL_DEPTH);
+    // c.glEnable(c.GL_BLEND);
+    // c.glBlendFunc(c.GL_SRC_ALPHA, c.GL_ONE_MINUS_SRC_ALPHA);
 
-	var chunks = ChunkManager.init(allocator);
-	defer chunks.deinit();
+    var chunks = ChunkManager.init(allocator);
+    defer chunks.deinit();
 
-	try chunks.addChunk();
+    try chunks.addChunk();
 
-	options.perspective = Matrix.perspective(
-		options.fov,
-		@as(FLOAT, @floatFromInt(options.width)) / @as(FLOAT, @floatFromInt(options.height)),
-		options.near,
-		options.far
-	);
-	c.glUniformMatrix4fv(
-		chunks.chunk_shader.uniform("projection"),
-		1,
-		c.GL_FALSE,
-		@ptrCast(&options.perspective[0])
-	);
+    options.perspective = Matrix.perspective(
+        options.fov,
+        @as(FLOAT, @floatFromInt(options.width)) / @as(FLOAT, @floatFromInt(options.height)),
+        options.near,
+        options.far
+    );
+    c.glUniformMatrix4fv(
+        chunks.chunk_shader.uniform("projection"),
+        1,
+        c.GL_FALSE,
+        @ptrCast(&options.perspective[0])
+    );
 
-	var x: FLOAT = 0;
+    var x: FLOAT = 0;
 
-	var prev = c.glfwGetTime();
-	while (c.glfwWindowShouldClose(window) == c.GL_FALSE) {
-		c.glClearColor(0.0, 0.0, 0.0, 1.0);
-		c.glClear(c.GL_COLOR_BUFFER_BIT);
+    var prev = c.glfwGetTime();
+    while (c.glfwWindowShouldClose(window) == c.GL_FALSE) {
+        c.glClearColor(0.0, 0.0, 0.0, 1.0);
+        c.glClear(c.GL_COLOR_BUFFER_BIT);
 
-		const timestamp = c.glfwGetTime();
-		const dt = timestamp - prev;
-		prev = timestamp;
+        const timestamp = c.glfwGetTime();
+        const dt = timestamp - prev;
+        prev = timestamp;
 
-		if (x > 45) x += 45.0 * @as(FLOAT, @floatCast(dt));
-		var rotate = Matrix.product(
-			options.perspective,
-			Matrix.xRotation(0)
-		);
-		rotate = Matrix.product(
-			rotate,
-			Matrix.zRotation(0)
-		);
-		c.glUniformMatrix4fv(
-			chunks.chunk_shader.uniform("projection"),
-			1,
-			c.GL_FALSE,
-			@ptrCast(&rotate[0])
-		);
+        if (x > 45) x += 45.0 * @as(FLOAT, @floatCast(dt));
+        var rotate = Matrix.product(
+            options.perspective,
+            Matrix.xRotation(0)
+        );
+        rotate = Matrix.product(
+            rotate,
+            Matrix.zRotation(0)
+        );
+        c.glUniformMatrix4fv(
+            chunks.chunk_shader.uniform("projection"),
+            1,
+            c.GL_FALSE,
+            @ptrCast(&rotate[0])
+        );
 
-   		c.glPolygonMode(c.GL_FRONT_AND_BACK, c.GL_LINE);
-		chunks.render();
+           c.glPolygonMode(c.GL_FRONT_AND_BACK, c.GL_LINE);
+        chunks.render();
 
-		c.glfwSwapBuffers(window);
-		c.glfwPollEvents();
-	}
+        c.glfwSwapBuffers(window);
+        c.glfwPollEvents();
+    }
 
-	c.glfwTerminate();
+    c.glfwTerminate();
 }
 
 fn resize(window: ?*c.GLFWwindow, width: c_int, height: c_int) callconv(.C) void {
-	_ = window;
-	c.glViewport(0, 0, width, height);
+    _ = window;
+    c.glViewport(0, 0, width, height);
 }
